4a204bd937183ce3d6c08e868f0a9cf544c8c713
Minhao Zhou D75Z <SE2321000016-D75Z@sll.se>
2025-06-30 15:23:46 +0200

    Specify the port

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import app  # import your Dash app object
+import os
 
 server = app.server  # Flask server instance
 
 if __name__ == "__main__":
-    server.run()
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port)
